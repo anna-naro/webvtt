9337b03df2743bfcc8721519303cc6cf032c175e
anna-naro <88615661+anna-naro@users.noreply.github.com>
2023-04-17 15:23:01 +0300

    Added logic ti change the subtitles
    - Added logic to handle 'add caption'.
    - Added logic to handle 'delete caption'.
    - Added logic to handle 'update caption'.
    - Added state management for active cue to display the current cue outside of the video.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,71 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
+  const trackRef = useRef({});
+  const videoRef = useRef();
+  const [activeCue, setActiveCue] = useState("");
+
+  useEffect(() => {
+    const track = trackRef.current;
+
+    track.addEventListener("cuechange", onCueChange);
+
+    return () => {
+      track.removeEventListener("cuechange", onCueChange);
+    };
+  }, []);
+
+  const addCaption = () => {
+    const cue = new VTTCue(8, 12, "this is a test");
+    trackRef.current.track.addCue(cue);
+  };
+
+  const deleteCaption = () => {
+    const cue = trackRef.current.track.cues[1];
+    trackRef.current.track.removeCue(cue);
+  };
+
+  const updateCaption = () => {
+    const cue = trackRef.current.track.cues[0];
+    cue.text = "my name is anna";
+  };
+
+  const onCueChange = () => {
+    const activeCues = trackRef.current.track.activeCues;
+    setActiveCue(activeCues[0]?.text || "");
+  };
+
   return (
-    <video id="video" controls width={900}>
-      <source
-        src="https://wsc-sports.video/k3y9"
-        type="video/mp4"
-      />
-      <track default kind="subtitles" src='subtitles.vtt' srclang="en" label='Subtitles'/>
-    </video>
+    <main style={{ display: "flex", flexDirection: "column" }}>
+      <video id="video" controls width={900} ref={videoRef}>
+        <source src="https://wsc-sports.video/k3y9" type="video/mp4" />
+        
+        <track
+          default
+          kind="subtitles"
+          src="subtitles.vtt"
+          srcLang="en"
+          label="Subtitles"
+          ref={trackRef}
+        />
+      </video>
+
+      <button onClick={addCaption}>
+        Add Caption (add text from 8 to 12 seconds)
+      </button>
+
+      <button onClick={deleteCaption}>
+        Delete Caption (delete text from 2 to 5 seconds)
+      </button>
+
+      <button onClick={updateCaption}>
+        Update Caption (update text from 0 to 2 seconds)
+      </button>
+
+      <div>
+        <p>Current text: {activeCue}</p>
+      </div>
+    </main>
   );
 }
 
